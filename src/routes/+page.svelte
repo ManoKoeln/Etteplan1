23dc3f8de28975297d478f71a2ed196415e5c868
Nowack Matthias <matthias.nowack@etteplan.com>
2024-11-12 09:08:33 +0100

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
+<script lang="ts">
+    import * as Menubar from "$lib/components/ui/menubar";
+    import { Button } from "$lib/components/ui/button";
+  </script>
+<header>
+    <h1>Page +page</h1>
+
+       
+      <Menubar.Root>
+        <Menubar.Menu>
+          <Menubar.Trigger>File</Menubar.Trigger>
+          <Menubar.Content>
+            <Menubar.Item>
+              New Tab
+              <Menubar.Shortcut>⌘T</Menubar.Shortcut>
+            </Menubar.Item>
+            <Menubar.Item>New Window</Menubar.Item>
+            <Menubar.Separator />
+            <Menubar.Item>Share</Menubar.Item>
+            <Menubar.Separator />
+            <Menubar.Item>Print</Menubar.Item>
+          </Menubar.Content>
+        </Menubar.Menu>
+      </Menubar.Root>
+</header>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+   
+  <Button>Click me</Button>
